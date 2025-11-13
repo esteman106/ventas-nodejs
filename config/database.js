26d9e8531d2e08697a20872f26a7beca2a582812
Manuel Romero <mromero106@gmail.com>
2025-11-13 17:49:23 -0500

    update data by default

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'inventarios',
+  process.env.DB_NAME || 'ventas',
   process.env.DB_USER || 'root',
   process.env.DB_PASSWORD || '',
   {
